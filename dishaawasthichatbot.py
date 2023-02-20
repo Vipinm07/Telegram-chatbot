2cbdbb7b3d42776263063d65c0966f69585aa85b
Vipinm07 <125807316+Vipinm07@users.noreply.github.com>
2023-02-20 09:20:58 +0530

    Update dishaawasthichatbot.py

--- dishaawasthichatbot.py
+++ dishaawasthichatbot.py
@@ -7,7 +7,7 @@ import openai
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
-    openai.api_key = "ENTER YOUR API KEY HERE"
+    openai.api_key = "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"
 else:
     openai.api_key = aienv
 print(aienv)
@@ -15,7 +15,7 @@ print(aienv)
 #Telegram bot key
 tgenv = os.getenv('TELEGRAM_KEY')
 if tgenv == None:
-    tgkey = "ENTER YOUR TELEGRAM TOKEN HERE"
+    tgkey = "6089174859:AAFG0z4CkQ9bPKUFhYkbgJ1jNysH8uFmsSQ"
 else:
     tgkey = tgenv
 print(tgenv)
